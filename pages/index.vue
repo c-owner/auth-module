0c36118eba864f9f7aa78650774be56170c8699e
corner <corner3499@gmail.com>
2022-12-20 21:10:30 +0900

    auth-module starter

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="hover:bg-black hover:text-white text-8xl">Index</h1>
+        <span class="badge text-sm">Test</span>
+    </div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="postcss" scoped>
+    .badge {
+        @apply bg-blue-500 text-white font-bold rounded-full px-3 py-1;
+    }
+</style>
---
+++ types/test.ts
@@ -0,0 +1,6 @@
+import test from "node:test"
+
+test("test", () => {
+// ...
+    const str: string = "test"
+})
